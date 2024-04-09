036c19f27cae0cad6a7c64e9161ac96961ce3603
Fabian Thien <166438716+thien-fab@users.noreply.github.com>
2024-04-09 21:00:12 +0200

    Default new WPF project

---
+++ cs2-scape/cs2-scape/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace cs2_scape;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
