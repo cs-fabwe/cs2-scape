3375ad27025d50cb162a8d67e0f6b98945acefa9
Fabian Thien <166438716+thien-fab@users.noreply.github.com>
2024-04-10 16:28:46 +0200

    Exploring html/css with razor pages

--- cs2-scape/cs2-scape/Components/Pages/Home.razor.cs
+++ cs2-scape/cs2-scape/Components/Pages/Home.razor.cs
@@ -6,4 +6,14 @@ namespace cs2_scape.Components.Pages;
 
 public partial class Home : ComponentBase
 {
+    private string Message { get; set; } = "Hello World!";
+    private int HelloCounter { get; set; }
+    private async Task ChangeMessageAsync()
+    {
+        await Task.Run(
+            () => HelloCounter++
+            );
+        Message = $"You said Hello {HelloCounter}x times!";
+        StateHasChanged();
+    }
 }
