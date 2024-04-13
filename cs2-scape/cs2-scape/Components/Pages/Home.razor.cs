4c4bc74cbf12b12adec6c4169d1cc6975e900e7f
Fabian Thien <166438716+thien-fab@users.noreply.github.com>
2024-04-14 00:27:30 +0200

    Further UI Testing/Experimenting

--- cs2-scape/cs2-scape/Components/Pages/Home.razor.cs
+++ cs2-scape/cs2-scape/Components/Pages/Home.razor.cs
@@ -1,13 +1,15 @@
-
-
+using System.Windows;
 using Microsoft.AspNetCore.Components;
 
 namespace cs2_scape.Components.Pages;
 
 public partial class Home : ComponentBase
 {
+
     private string Message { get; set; } = "Hello World!";
     private int HelloCounter { get; set; }
+    private bool ShowSelection { get; set; } = false;
+    private string SelectionMenuClass { get; set; } = "selection-menu-inactive";
     private async Task ChangeMessageAsync()
     {
         await Task.Run(
@@ -15,5 +17,16 @@ public partial class Home : ComponentBase
             );
         Message = $"You said Hello {HelloCounter}x times!";
         StateHasChanged();
+    }
+
+    private void ToggleFileSelection()
+    {
+        ShowSelection = !ShowSelection;
+        SelectionMenuClass = ShowSelection ? "selection-menu" : "selection-menu-inactive";
+        StateHasChanged();
+    }
+    private void OpenFileDialogForFolders()
+    {
+        MessageBox.Show("Open a file dialog here");
     }
 }
