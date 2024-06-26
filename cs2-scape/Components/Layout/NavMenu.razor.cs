9e3eefd75009f94c3d63ddff033bde811da146bc
Fabian <166438716+th-fab@users.noreply.github.com>
2024-06-26 16:22:26 +0200

    Updated navigation
    Found out about navigation manager and testing it now

---
+++ cs2-scape/Components/Layout/NavMenu.razor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cs2_scape.Components.Layout;
+
+partial class NavMenu
+{
+	public void GoToPage(string page)
+	{
+		Navigation.NavigateTo(page);
+	}
+}
