a7a09509bef5d675936aca490a2bd77ade0f2af4
Fabian <166438716+th-fab@users.noreply.github.com>
2024-06-30 19:11:57 +0200

    fixed css, removed unused imports

--- cs2-scape/Components/Layout/NavMenu.razor.cs
+++ cs2-scape/Components/Layout/NavMenu.razor.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace cs2_scape.Components.Layout;
 
-namespace cs2_scape.Components.Layout;
-
-partial class NavMenu
+public partial class NavMenu
 {
 	public void GoToPage(string page)
 	{
